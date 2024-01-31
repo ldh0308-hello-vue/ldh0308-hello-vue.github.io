606c6060448cb1fd58de76a9feb5a4abbd3e065a
donghyuk <ldh0308@naver.com>
2024-01-31 14:05:32 +0900

    if

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
 import { ref } from 'vue'
-const message = ref({name: "dong", age: 34})
+
+const awesome = ref(true)
+
+function toggleAwesome(){
+    awesome = !awesome
+}
+
+function isAwesome(){
+    if(true){
+	console.log("Vue is awesome!")
+    } else { console.log("On no")
+    }
+}
 </script>
 
 <template>
- <h1>{{ message }}</h1>
+ <button @click="toggleAwesome">toggle</button>
+ <br>
+ <h1 v-if="isAwesome">{{ isAwesome() }}</h1>
+
+ <h4 v-if="awesome">TRUE</h4>
+ <h4 v-show="awesome">TRue</h4>
 </template>
 
