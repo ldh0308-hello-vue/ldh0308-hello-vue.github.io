0773b0ac2d8b13eec33bbb2aadcca7a23384af45
donghyuk <ldh0308@naver.com>
2024-01-31 18:58:39 +0900

    단어검색 시도 중

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,17 @@ import science from "@/assets/science.json"
 const news = ref(science.articles)
 //const parentMessage = ref('Parent')
 //const items = ref([{ message: 'Foo' }, { message: 'Bar'}])
+function keyword(author){
+    return author.includes('n')
+}
 </script>
 
 <template>
   <ul>
    <template v-for= "(jsonItem) in news">
-   <li v-if="jsonItem.author === '이지운'">
+   <li v-if="keyword(jsonItem.author)">
      {{ jsonItem.author }} <br>
-     {{ jsonItem.title }} - {{ jsonItem.name}}
+    {{jsonItem.title}} - {{ jsonItem.name}}
    </li>
    </template>
   </ul>
