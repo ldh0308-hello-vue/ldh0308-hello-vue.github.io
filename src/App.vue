cb3d69e544f489b224aad153956bf1f418f733d6
donghyuk <ldh0308@naver.com>
2024-01-31 14:48:16 +0900

    for

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 
-const awesome = ref(true)
-
-function toggleAwesome(){
-    awesome = !awesome
-}
-
-function isAwesome(){
-    if(true){
-	console.log("Vue is awesome!")
-    } else { console.log("On no")
-    }
-}
+const parentMessage = ref('Parent')
+const items = ref([{ message: 'Foo' }, { message: 'Bar'}])
 </script>
 
 <template>
- <button @click="toggleAwesome">toggle</button>
- <br>
- <h1 v-if="isAwesome">{{ isAwesome() }}</h1>
-
- <h4 v-if="awesome">TRUE</h4>
- <h4 v-show="awesome">TRue</h4>
+  <li v-for= "(item, index) in items">
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>
 </template>
-
